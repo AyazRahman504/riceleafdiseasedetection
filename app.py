8c16fa5507aa491b307ce94728269a0b6f490545
Ayaz Rahman Bhuiyan <81004595+AyazRahman504@users.noreply.github.com>
2024-12-27 13:37:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -433,9 +433,9 @@ yolov11_folder = 'yolov11'
 
 # Dictionary of model analytics images folder paths
 model_results_paths = {
-    "YOLOv8": r"Models_&_Results\Yolo_v8\Result",
-    "YOLOv10": r"Models_&_Results\Yolo_v10\Result",
-    "YOLOv11": r"Models_&_Results\Yolo_v11\Result",
+    "YOLOv8": r"Models_and_Results\Yolo_v8\Result",
+    "YOLOv10": r"Models_and_Results\Yolo_v10\Result",
+    "YOLOv11": r"Models_and_Results\Yolo_v11\Result",
 }
 
 # Ensure all folders are created once
